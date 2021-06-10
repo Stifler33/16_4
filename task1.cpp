fd16805a084f9325f01a56b14b8b9c49c28b9ddb
stifler33 <bagi33.ru@gmail.com>
2021-06-10 23:14:33 +0300

    Initial commit

---
+++ task1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+using namespace std;
+float speed = 0;
+bool permissibleSpeed(float delta){
+   return speed <= 0 - delta;
+}
+int main() {
+    float delta;
+    char text[100];
+    do{
+        cin >> delta;
+        if (delta + speed <= 150) {
+            speed += delta;
+            sprintf(text, "Speed %.2f", speed);
+            cout << text << endl;
+        }else cout << "over speed !\n";
+    }while(!permissibleSpeed(0.01));
+
+    return 0;
+}
