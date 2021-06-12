5aa41f3f24d051c06570a590c153a86a11962d09
stifler33 <bagi33.ru@gmail.com>
2021-06-12 15:11:52 +0300

    task 3 init

---
+++ task3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+
+int main(){
+    std::string userAns;
+    std::cin >> userAns;
+    std::stringstream buffer;
+    buffer << userAns;
+    double first;
+    double second;
+    char sing;
+    buffer >> first >> sing >> second;
+    std::cout << first << " " << sing << " " << second << std::endl;
+    switch (sing) {
+        case'+':
+            std::cout << first + second;
+            break;
+        case'-':
+            std::cout << first - second;
+            break;
+        case'*':
+            std::cout << first * second;
+            break;
+        case'/':
+            std::cout << first / second;
+            break;
+        default :
+            std::cout << "Fail!\n";
+    }
+    return 0;
+}
