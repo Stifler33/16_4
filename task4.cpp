887b6725da8c644e7943dc7dcd1faade3a762844
stifler33 <bagi33.ru@gmail.com>
2021-06-12 18:24:23 +0300

    task 4 initial

---
+++ task4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+enum note{
+    A = 'A',
+    B,
+    C,
+    D,
+    E,
+    F,
+    G
+};
+int main(){
+    std::stringstream melody;
+    /*
+    for (int i = 0; i < 12; i++){
+        int num;
+        std::cin >> num;
+        melody << num << ' ';
+    }
+     */
+    char s;
+    s |= G;
+    std::cout <<  ;
+    int state = 0;
+    state &= F;
+
+    return 0;
+}
